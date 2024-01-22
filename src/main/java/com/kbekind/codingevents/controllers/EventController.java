b859e9514f1db86e8b71af39bbdc86d0d832e414
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-01-22 10:18:25 -0600

    commit

--- src/main/java/com/kbekind/codingevents/controllers/EventController.java
+++ src/main/java/com/kbekind/codingevents/controllers/EventController.java
@@ -1,31 +1,24 @@
 package com.kbekind.codingevents.controllers;
 
+import com.kbekind.codingevents.data.EventData;
+import com.kbekind.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-import java.util.HashMap;
-
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-  private static HashMap<String, String> events = new HashMap<>();
-
-  public EventController() {
-    events.put("Menteaship","A fun meetup for connecting with mentors");
-    events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
-    events.put("Javascripty", "An imaginary meetup for Javascript developers");
-  }
-
+//  public EventController() {
+//    events.add("Menteaship A fun meetup for connecting with mentors");
+//    events.add("Code With Pride A fun meetup sponsored by LaunchCode");
+//    events.add("Javascripty An imaginary meetup for Javascript developers");
+//  }
+//
   @GetMapping
   public String displayAllEvents(Model model) {
-    model.addAttribute("events", events);
+    model.addAttribute("events", EventData.getAll());
     return "events/index";
   }
 
@@ -35,10 +28,44 @@ public class EventController {
   }
 
   @PostMapping("create")
-  public String createEvent(@RequestParam String eventName, @RequestParam String eventDesc){
-    events.put(eventName, eventDesc);
+  public String processCreateEventForm(@ModelAttribute Event newEvent){
+
+    EventData.add(newEvent);
     return "redirect:/events";
   }
 
+  @GetMapping("delete")
+  public String displayDeleteEventForm(Model model){
+    model.addAttribute("title", "Delete Events");
+    model.addAttribute("events", EventData.getAll());
+    return "events/delete";
+  }
+
+  @PostMapping("delete")
+  public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds){
+    for (int id: eventIds){
+      EventData.remove(id);
+    }
+    return "redirect:/events";
+  }
+
+  @GetMapping("edit/{eventId}")
+  public String displayEditForm(Model model, @PathVariable int eventId){
+
+   model.addAttribute("event" ,EventData.getById(eventId));
+    return "events/edit";
+
+  }
+
+  @PostMapping("edit")
+  public String processEditForm(int eventId, String name, String description){
+
+    Event editedEvent = EventData.getById(eventId);
+    editedEvent.setName(name);
+    editedEvent.setDescription(description);
+
+    return "redirect:/events";
+
+  }
 
 }
